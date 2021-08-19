9811299a6e5d01581b2b4518eda2bf8be554dd41
Animesh Bala Ani <animesh.ani@live.com>
2021-08-19 05:56:49 -0400

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -6,7 +6,6 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 python3 -m pip install setuptools cffi git+https://github.com/ipython/traitlets@4.x jupyter jupyterlab
 
-export PATH=$PATH:~/.local/bin
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter labextension install @jupyterlab/statusbar
 jupyter lab --generate-config
