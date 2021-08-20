555d21facc05491bc99a29171a4fc8f22b4de70b
Animesh Bala Ani <animesh.ani@live.com>
2021-08-20 09:19:46 -0400

    Create dashing.sh

---
+++ dashing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Animesh Bala Ani (ANI717)
+
+locale  # check for UTF-8
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale  # verify settings
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt install ros-dashing-desktop
+
+source /opt/ros/dashing/setup.bash
+echo  'source /opt/ros/dashing/setup.bash' >> ~/.bashrc 
