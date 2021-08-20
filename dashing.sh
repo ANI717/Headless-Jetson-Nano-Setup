9791045535e990df9281f6de6f13b8848a58bc80
Animesh Bala Ani <animesh.ani@live.com>
2021-08-20 09:23:21 -0400

    Update dashing.sh

--- dashing.sh
+++ dashing.sh
@@ -8,7 +8,7 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
 
-sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo apt update && sudo apt install -y curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
