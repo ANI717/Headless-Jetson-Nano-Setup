f7f84f0655c7006d4f1cf1ec07d0aaf2f4fa9f95
Animesh Bala Ani <animesh.ani@live.com>
2021-08-20 08:52:52 -0400

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Animesh Bala Ani (ANI717)
 
-cd ~
 sudo apt update
 sudo apt install -y python3-pip curl libffi-dev
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
@@ -18,7 +17,6 @@ echo "c.NotebookApp.token = ''" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.password_required = True" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.allow_credentials = False" >> $HOME/.jupyter/jupyter_lab_config.py
 
-cd ~/headless_jetson_nano_setup
 python3 create_jupyter_service.py
 sudo mv jetbot_jupyter.service /etc/systemd/system/jetbot_jupyter.service
 sudo systemctl enable jetbot_jupyter
