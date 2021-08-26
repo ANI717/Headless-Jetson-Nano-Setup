abb82dcd4a8a74e6f95dc4573d5f7aacbb411684
Animesh Bala Ani <animesh.ani@live.com>
2021-08-26 08:27:45 -0400

    Update dashing.sh

--- dashing.sh
+++ dashing.sh
@@ -12,7 +12,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
-sudo apt install -y ros-dashing-desktop
+sudo apt install -y ros-dashing-desktop python3-colcon-common-extensions
 
 source /opt/ros/dashing/setup.bash
 echo  'source /opt/ros/dashing/setup.bash' >> ~/.bashrc 
