e9700fff2ee26bf8e09f80077c6d051272db1a6a
Animesh Bala Ani <animesh.ani@live.com>
2021-11-13 01:49:41 -0500

    Create pytorch.sh

---
+++ pytorch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Animesh Bala Ani (ANI717)
+
+# exit immediately if a command exits with a non-zero status.
+set -e
+
+# record script start time
+date
+
+# install dependency
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-setuptools libopenblas-base libopenmpi-dev
+sudo -H python3 -m pip install --upgrade testresources setuptools protobuf
+sudo -H pip3 install numpy==1.19.4
+
+# install torch and torchvision
+wget https://nvidia.box.com/shared/static/h1z9sw4bb1ybi0rm3tu8qdj8hs05ljbm.whl -O torch-1.9.0-cp36-cp36m-linux_aarch64.whl
+sudo -H python3 -m pip install torch-1.9.0-cp36-cp36m-linux_aarch64.whl torchvision
+
+# record script end time
+date
