813d6e415959ea53ae0d9be127432296006cb31b
Animesh Bala Ani <animesh.ani@live.com>
2021-11-12 23:46:54 -0500

    Update create_stats_service.py

--- create_stats_service.py
+++ create_stats_service.py
@@ -9,7 +9,7 @@ Description=JetBot stats display service
 [Service]
 Type=simple
 User=%s
-ExecStart=/bin/sh -c "python3 -m jetbot.apps.stats"
+ExecStart=/bin/sh -c "python3 -m stats"
 WorkingDirectory=%s
 Restart=always
 
