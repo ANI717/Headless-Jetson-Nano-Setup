95cac78ead87d6232bd5422c4ae82527f0e24279
Animesh Bala Ani <animesh.ani@live.com>
2021-11-15 02:24:00 -0500

    Update torch2trt_onnx.sh

--- torch2trt_onnx.sh
+++ torch2trt_onnx.sh
@@ -15,7 +15,8 @@ sudo -H python3 setup.py install --plugins
 echo  'alias trtexec="/usr/src/tensorrt/bin/trtexec"' >> ~/.bashrc 
 
 # install onnx and onnxruntime
-sudo -H pip3 install onnx onnxruntime
+wget https://nvidia.box.com/shared/static/8sc6j25orjcpl6vhq3a4ir8v219fglng.whl -O onnxruntime_gpu-1.4.0-cp36-cp36m-linux_aarch64.whl
+sudo -H pip3 install onnx onnxruntime_gpu-1.4.0-cp36-cp36m-linux_aarch64.whl
 
 # set numpy back to supported version 
 sudo -H pip3 install numpy==1.19.4
